03f6ca90729fa5b818324c70046f323a4b07b42a
Анастасия Кечкина <kechnastya@gmail.com>
2023-07-18 10:02:53 +0300

    check that everything works

--- src/capitalize.js
+++ src/capitalize.js
@@ -2,3 +2,4 @@ export const capitalize = (text) => {
    const [firstSymbol = '', ...restSymbols] = text;
    return `${firstSymbol.toUpperCase()}${restSymbols.join('')}`;
  };
+
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,11 @@
 import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
+  throw new Error('Кто-то сломал наш тест!');
 }
 
 if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
+  throw new Error('Сегодня вы не сладкая булочка :(');
 }
 
 console.log('Поздравляю! Вы - сладкая булочка!');
